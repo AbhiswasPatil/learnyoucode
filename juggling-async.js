935439a2202e3c79171e29eb51d118d519555cf0
Abhishek Patil <abhiswaspatil1710@gmail.com>
2021-06-14 21:32:43 +0530

    juggling async with 2 methods

--- juggling-async.js
+++ juggling-async.js
@@ -1,23 +1,28 @@
 const http = require("http");
-const bl = require("bl");
 const urlList = process.argv.slice(2);
 
 let totalData = [];
 let counter = 0;
+
+for (let i = 0; i < urlList.length; i++) {
+  totalData[i] = "";
+}
+
 for (let i = 0; i < urlList.length; i++) {
   http.get(urlList[i], (response) => {
-    response.pipe(
-      bl((err, data) => {
-        if (err) {
-          return console.log(error);
+    response.setEncoding("utf-8");
+
+    response.on("data", (chunk) => {
+      totalData[i] += chunk;
+    });
+
+    response.on("end", () => {
+      counter++;
+      if (counter === urlList.length) {
+        for (let i = 0; i < urlList.length; i++) {
+          console.log(totalData[i]);
         }
-        data = data.toString();
-        totalData[i] = data;
-        counter++;
-        if (counter === urlList.length) {
-          totalData.forEach((data) => console.log(data));
-        }
-      })
-    );
+      }
+    });
   });
 }
